87c97f9f0a9ce38b812493dd39a41f8850aee776
Alex Dev Sm <leshasmlesha@gmail.com>
2025-01-08 21:43:30 +0300

    Fixed: поправил на git репозиторий

--- tools/build_qtwebkit.bat
+++ tools/build_qtwebkit.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set EXTPATH=%SRCDIR%\%EXTNAME%-opensource-src-%QTWEBKIT_VER%
+set EXTPATH=%SRCDIR%\%EXTNAME%-%QTWEBKIT_VER%
 set QMAKE=%QTINSTALLDIR%\bin\qmake.exe
 
 IF NOT "%EXTNAME%" == "" (
@@ -9,25 +9,11 @@ IF NOT "%EXTNAME%" == "" (
 	echo PATH: %EXTPATH%
 	echo QMAKE: %QMAKE%
 
-	set URL=https://mirror.netcologne.de/qtproject/snapshots/ci/%EXTNAME%/%QTWEBKIT_VER%/latest/src/submodules/%EXTNAME%-opensource-src-%QTWEBKIT_VER%.zip
-
-	cd %SRCDIR%
+	set URL=https://github.com/qtwebkit/qtwebkit.git
+	mkdir %EXTPATH%
+	cd %EXTPATH%
 	echo Downloading !URL!
-	curl %CURLOPTS% !URL!
-
-	rd %EXTPATH% /s /q
-	7z %ZOPTS% %EXTNAME%-opensource-src-%QTWEBKIT_VER%.zip || exit /b %errorlevel%
-	cd ..
-	
-	copy /y Tools\patches\qtwebkit\build-qtwebkit-conan.py %EXTPATH%\Tools\qt\build-qtwebkit-conan.py
-	copy /y Tools\patches\qtwebkit\conan_dependencies_version.py %EXTPATH%\Tools\qt\conan_dependencies_version.py
-	copy /y Tools\patches\qtwebkit\conanfile.py %EXTPATH%\Tools\qt\conanfile.py
-	copy /y Tools\patches\qtwebkit\FindLibXslt.cmake %EXTPATH%\Source\cmake\FindLibXslt.cmake
-	copy /y Tools\patches\qtwebkit\FindLibXml2.cmake %EXTPATH%\Source\cmake\FindLibXml2.cmake
-	copy /y Tools\patches\qtwebkit\FindICU.cmake %EXTPATH%\Source\cmake\FindICU.cmake
-	copy /y Tools\patches\qtwebkit\FindJPEG.cmake %EXTPATH%\Source\cmake\FindJPEG.cmake
-	copy /y Tools\patches\qtwebkit\OptionsQt.cmake %EXTPATH%\Source\cmake\OptionsQt.cmake
-	copy /y Tools\patches\qtwebkit\PlatformQt.cmake %EXTPATH%\Source\WebKit\PlatformQt.cmake
+	git clone --depth 10 -b qtwebkit-5.212 !URL! .
 
 	cd %EXTPATH% ||  exit /b %errorlevel%
 
